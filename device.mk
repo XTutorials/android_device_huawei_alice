3744dd7ad95ac17c5aaf205d0599f18652cb937c
kosmichak <kosmitchak@gmail.com>
2017-10-25 18:15:09 +0200

    use stock codecs, see this: https://github.com/kosmitchak/android_vendor_huawei_alice/commit/8aff0d8190baca6494a221af1d8beccf93b4817d

--- device.mk
+++ device.mk
@@ -55,12 +55,6 @@ PRODUCT_PACKAGES += \
 # Blobs
 $(call inherit-product-if-exists, vendor/huawei/alice/alice-vendor.mk)
 
-# Codecs
-PRODUCT_COPY_FILES += \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-
 # Dalvik
 PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
